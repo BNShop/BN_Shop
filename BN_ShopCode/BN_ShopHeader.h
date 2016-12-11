8d45ec032c49b916574a082c7e06705c9c0caca8
xuweikai <xuweikai@xiaoyouzi.com>
2016-12-11 22:51:02 +0800

    Fix

---
+++ BN_ShopCode/BN_ShopHeader.h
@@ -0,0 +1,16 @@
+//
+//  BN_ShopHeader.h
+//  BN_Shop
+//
+//  Created by 许为锴 on 2016/12/11.
+//  Copyright © 2016年 GL_RunMan. All rights reserved.
+//
+
+#ifndef BN_ShopHeader_h
+#define BN_ShopHeader_h
+
+#import "QJTL_Global.h"
+
+#define Shop_BASEURL @"http://112.74.31.159:26088/lbb-app"//@"http://169fa47.ittun.com/lbb"//
+
+#endif /* BN_ShopHeader_h */
